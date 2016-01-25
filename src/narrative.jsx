7d8e975a0994e516509618d3b15932ccd5b85b5e
James Chuang <james.chuang@socrata.com>
2016-01-25 05:50:11 -0800

    switch back to es6 syntax and add more prop options

--- src/narrative.jsx
+++ src/narrative.jsx
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 
-var Narrative = React.createClass({
+class Narrative extends React.Component {
   render() {
     return <h1>Welcome to Visualizations on React.js</h1>
   }
-});
+}
 
 render(<Narrative/>, document.getElementById('narrative'));
--- src/pie-chart.jsx
+++ src/pie-chart.jsx
@@ -2,12 +2,38 @@ import React, { Component } from 'react';
 import { render } from 'react-dom';
 import { VictoryPie } from 'victory';
 
-var PieChart = React.createClass({
+class PieChart extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {data: this.getData()};
+  }
+
+  getData() {
+    return [
+      { x: "A", y: 0.2 + Math.random() },
+      { x: "B", y: 0.2 + Math.random() },
+      { x: "C", y: 0.2 + Math.random() },
+      { x: "D", y: 0.2 + Math.random() },
+      { x: "E", y: 0.2 + Math.random() }
+    ];
+  }
+
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({data: this.getData()});
+    }, 1000);
+  }
+
   render() {
     return (
-      <VictoryPie />
+      <VictoryPie
+        data={this.state.data}
+        animate={{velocity: 0.01}}
+        innerRadius={80}
+        padAngle = {5}
+        colorScale={['#eff3ff','#bdd7e7','#6baed6','#3182bd','#08519c']}/>
     );
   }
-});
+}
 
 render(<PieChart />, document.getElementById('pie-chart'));
