d6e03b77ba1695a542b951eb3625a0bf18b5abf5
James Chuang <james.chuang@socrata.com>
2016-01-25 03:49:49 -0800

    add narrative component

---
+++ src/narrative.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class Narrative extends React.Component {
+  render() {
+    return <h1>Welcome to Visualizations on React.js</h1>
+  }
+}
+
+React.render(<Narrative/>, document.getElementById('narrative'));
